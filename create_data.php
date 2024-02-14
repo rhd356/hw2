a1d9f0df5b609f6fb3cfc4aa00010537e7878f8b
MaraDoze <122954800+MaraDoze@users.noreply.github.com>
2024-02-13 18:35:04 -0800

    Update create_data.php

--- create_data.php
+++ create_data.php
@@ -6,7 +6,40 @@
     </head>
     <body>
         <?php
-            
+            //making an array to store the first names in the file
+            $firstName = file('first_names.cvs');
+            echo "First Name";
+            echo '<pre>';
+            print_r($firstName);
+            echo '</pre>';
+        //making an array to store the last names in the file
+            $lastName = file('last_names.txt');
+            echo "Last Name";
+            echo '<pre>';
+            print_r($lastName);
+            echo '</pre>';
+        //making an array to store the street names in the file
+            $streetName = file('street_names.txt');
+            echo "Street Name";
+            echo '<pre>';
+            print_r($streetName);
+            echo '</pre>';
+        //making an array to store the street type in the file
+            $streetType = file('street_types.txt');
+            echo "Street Type";
+            echo '<pre>';
+            print_r($lastName);
+            echo '</pre>';
+        //making an array to store the domain in the file
+            $domainFile = file('domain.txt');
+            echo "Domain";
+            echo '<pre>';
+            print_r($domainFile);
+            echo '</pre>';
+        //domain field
+        //
+
+
         ?>
     </body>
 </html>
